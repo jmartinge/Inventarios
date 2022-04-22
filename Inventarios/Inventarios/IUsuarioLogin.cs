e6d211650b7d6d9971495a1be46ed3363e0207bf
jmartinge <jmartingonzalezestevez@gmail.com>
2022-04-22 16:09:36 -0500

    Agregar archivos de proyecto.

---
+++ Inventarios/Inventarios/IUsuarioLogin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Refit;
+
+namespace Inventarios
+{
+    public interface IUsuarioLogin
+    {
+        [Get("/_controllers/CredencialesController.php?instrucion=0&ID_USUARIO=PLJC01&PASSWORD=57CF06A59E2D9DBEB946D2BFF2C26A0C")]
+        Task<string> GetUsuarioAsync(string ID_USUARIO, string PASSWORD, int instrucion = 0);
+    }
+}
---
+++ Inventarios/Inventarios/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Inventarios
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(ID_USUARIO.Text) && !string.IsNullOrEmpty(PASSWORD.Text))
+            {
+                var getApi = RestService.For<IUsuarioLogin>("https://sirvamonos.com/examen");
+                var resultado = await getApi.GetUsuarioAsync(ID_USUARIO.Text, PASSWORD.Text);
+            }
+            else
+            {
+                _ = DisplayAlert("Ingresare datos", "Se requiere Usaurio y Contraseña", "Ok");
+
+            }
+        }
+    }
+}
